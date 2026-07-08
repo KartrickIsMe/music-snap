61d39c4c06e8054170f924fa774ac984b0913e7d
KartrickIsMe <kartikeys860@gmail.com>
2026-07-08 23:27:15 +0530

    Half Rewrite

--- android/app/src/main/java/com/echoai/musicsnap/MainActivity.java
+++ android/app/src/main/java/com/echoai/musicsnap/MainActivity.java
@@ -1,19 +1,32 @@
 package com.echoai.musicsnap;
 
 import com.getcapacitor.BridgeActivity;
+
+import android.annotation.SuppressLint;
 import android.webkit.JavascriptInterface;
 import android.os.Bundle;
 
 import com.yausername.youtubedl_android.YoutubeDL;
 public class MainActivity extends BridgeActivity {
 
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            YoutubeDL.getInstance().init(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread( () -> {
+            getBridge().getWebView().addJavascriptInterface(this, "Android");
+            try {
+                YoutubeDL.getInstance().init(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                getBridge().getWebView().evaluateJavascript("onInitialized()" , null);
+            }
+        }).start();
+    }
+    @JavascriptInterface
+    public void downloadToCache() {
+
     }
 }
--- www/app.js
+++ www/app.js
@@ -1,29 +1,100 @@
-const urlInput = document.getElementById("urlInput");
-const audioPlayer = document.getElementById("audioPlayer");
-const status = document.getElementById("status");
-
 window.addEventListener("error" , logJsError);
 
-function onButtonClick() {s
+const urlInput = document.getElementById("urlInput");
+const audioPlayer = document.getElementById("audioPlayer");
+const statusDiv = document.getElementById("statusDiv");
+const loadAudio = document.getElementById("loadAudio");
+const saveAudio = document.getElementById("saveAudio");
+const downloadAudio = document.getElementById("downloadAudio");
+
+logEventReplace("JsReady" , false);
+
+function disableAllButtons() {
+    downloadAudio.disabled = true;
+    loadAudio.disabled = true;
+    saveAudio.disabled = true;
 }
 
+disableAllButtons();
 
+function onDownloadClick(url) {
+    url = urlInput.value;
+    window.Android.downloadToCache();
+}
 
-
-
-
-
-
+function onSaveClick() {
+    window.Android.saveToMusic();
+}
 
 
 function logJsError(e) {
-    let line = document.createElement("p");
-    line.textContent = e.message + "\n";
-    status.appendChild(line);
+    logEventReplace(e.message, true);
 }
 
-function logEvent(event) {
+function logEvent(event, isError) {
     let line = document.createElement("p");
     line.textContent = event;
-    status.appendChild(line);
+    if (isError){
+        line.style.color = "red";
+    }
+    statusDiv.appendChild(line);
 }
+
+function logEventReplace(event, isError) {
+    let line = document.createElement("p");
+    line.textContent = event;
+    if (isError == true) {
+        line.style.color = "red";
+    }
+    else if (isError == false) {
+        line.style.color = "green"
+    }
+    else {
+        line.style.color = "black";
+    }
+    statusDiv.innerHTML = "";
+    statusDiv.appendChild(line);
+}
+
+window.onInitialized = function () {
+    enableAllButtons();
+}
+
+/*try {
+    const { Capacitor } = require("@capacitor/core");
+}
+catch(e) {
+    logEventReplace("[Capacitor] Environment is Incorrect : " + e.message, true);
+    disableAllButtons();
+
+}*/
+
+function onLoadClick(nativeFilePath) {
+    try {
+        webFilePath = Capacitor.convertFileSrc(nativeFilePath);
+        audioPlayer.src = webFilePath;
+        audioPlayer.load();
+    }
+    catch(e) {
+        logEventReplace(e.message, true);
+    }
+}
+
+function wait(timeMs) {
+    return new Promise();
+}
+
+function enableAllButtons() {
+    downloadAudio.disabled = false;
+    loadAudio.disabled = false;
+    saveAudio.disabled = false;
+}
+
+downloadAudio.addEventListener("click", onDownloadClick);
+saveAudio.addEventListener("click", onSaveClick);
+loadAudio.addEventListener("click", onLoadClick);
+
+
+
+
+
